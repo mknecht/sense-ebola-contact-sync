4eb25a8a9887943e7e0c7cdc8a0c29319d0d011b
Murat Knecht <murat.knecht@gmx.de>
2014-09-16 15:04:50 +0200

    Adding row to control the 'is-duplicate' markers on contacts.

--- app/scripts/controllers/deduplicate.js
+++ app/scripts/controllers/deduplicate.js
@@ -15,12 +15,33 @@ secsApp
       function ($scope, $filter, NgTableParams, contactFactory, SETTINGS) {
 
         contactFactory.allOrderedByName().then(function(contacts) {
+          function extendContact(contact) {
+            // Ugly extension of Contact objects, because contacts are
+            // plain objects, i.e. do not share a prototype.
+            // Functions would be more ugly, because we're really working
+            // on the objects, hence methods are appropriate and also
+            // we'd have to pass the functions to the isolated scope.
+            if (contact.isDuplicate === undefined) {
+              contact.isDuplicate = function() {
+                return this.duplicateOf !== undefined
+              }
+            }
+            if (contact.markAsDuplicate === undefined) {
+              contact.markAsDuplicate = function(originalContact) {
+                return this.duplicateOf = originalContact._id
+              }
+            }
+          }
+
           // TODO Choose a suitable pair.
           $scope.left = contacts[4];
           $scope.right = contacts[5];
 
+          extendContact($scope.left)
+          extendContact($scope.right)
+
           // TODO Test data to be removed
-          $scope.left.isduplicate = true
+          $scope.left.duplicateOf = "3e96add5cbba0bcd9345745637017a4h"
         });
 
         $scope.toggleStatus = function(contact, event) {
@@ -48,5 +69,16 @@ secsApp.directive('mergeString', function() {
       title: '@'
     },
     templateUrl: 'views/deduplicate_string.html'
+  }
+})
+
+secsApp.directive('duplicateMarker', function() {
+  return {
+    restrict: 'A',
+    scope: {
+      other: '=',
+      this: '='
+    },
+    templateUrl: 'views/deduplicate_marker.html'
   }
 })
